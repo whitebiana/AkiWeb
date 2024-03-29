ff01b04d927d3eda3bbd96d17f702a27a43292e7
cym <whitebiana@gmail.com>
2024-03-29 21:14:41 +0800

    wip: 初始化后端

---
+++ aki-web-backend/src/main/java/pub/akiwebbackend/AkiWebBackendApplication.java
@@ -0,0 +1,13 @@
+package pub.akiwebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkiWebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkiWebBackendApplication.class, args);
+    }
+
+}
---
+++ aki-web-backend/src/test/java/pub/akiwebbackend/AkiWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pub.akiwebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkiWebBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
